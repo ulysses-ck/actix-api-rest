d31dfa35ed4b6730e394e4be36f64d6d447b8605
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-17 23:50:13 -0300

    feat(api): add simple route /users to retrieve a fake list of users

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+// create a route /users
+// and retrieve an array of fake users with a GET request
+#[get("/users")]
+async fn get_users() -> impl Responder {
+    let users = vec!["John", "Jane", "Joe"];
+    HttpResponse::Ok().json(users)
+}
+
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -20,6 +29,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(get_users)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
